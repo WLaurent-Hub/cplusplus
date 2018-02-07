d5fabf33778c45e8ce308263ef4abe98a9e9b99c
eleve <eleve@si-PC11>
2018-02-07 14:47:38 +0100

    Nous avons fini exo1

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -5,56 +5,17 @@
 #define MY_HEADER_HPP  
 #include <cmath>
 
-double exo1(double rayon)
+void exo1(void)
 {
-	double volume;
-	volume = 4/3*3.14 * rayon * rayon* rayon ;
-	return volume;
-}
-void exo2(short a, short b)
-{
-	short y;
-	y = a < b && !a;
-}
-double exo3(double rayon)
-{
-	double aire;
-	aire = pow(rayon, 3)*3.14 * 4 / 3;
-	return aire;
-}
-double exo4(short a, short b, short c)
-{
-	short y;
-	y = 0;
-	if (a > b)
+	long a = 2, b = 1, c = 0;
+	while (a + b > 0)
 	{
-		if (a > c)
-			y = a;
-		else
-			y = c;
+		a = a - 2;
+		b = b + 1;
+		c = c + 1;
 	}
-	else
-	{
-		if (b > c)
-			y = b;
-		else
-			y = c;
-	}
-	return y;
 }
-double exo5(double prix)
-{
-	short y;
-	y = 0;
-	if (prix > 100)
-		y = 0.05 * prix;
-	{
-		if (prix > 300)
-			y = 0.08*prix;
-	}
-	
-	
-	return prix - y;
+
 
 
 }
